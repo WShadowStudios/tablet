7db8f8cc926f888753965f101315da1145a59dd0
WShadowStudios <129547469+WShadowStudios@users.noreply.github.com>
2025-02-02 20:08:41 +0300

    Add files via upload

---
+++ normal_tablet.lua
@@ -0,0 +1,7 @@
+-- modded upio scanner :omg:
+
+--local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
+
+---------------------------------------------------------------------------------------------------------------------------
+local Scanner = LoadCustomInstance("https://raw.githubusercontent.com/WShadowStudios/tablet/main/original_tablet.rbxm")
+Scanner.Parent = game.Players.LocalPlayer.Backpack
