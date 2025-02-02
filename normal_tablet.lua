b733291d8b643fffa886e275bdc27c9090ae913a
WShadowStudios <129547469+WShadowStudios@users.noreply.github.com>
2025-02-02 20:16:23 +0300

    Update normal_tablet.lua

--- normal_tablet.lua
+++ normal_tablet.lua
@@ -1,335 +1,242 @@
--- modded upio scanner :omg:
+-- Code by LSPLASH :scream:
+-- Use _G.OnShop = false if you are using Delta
 
-local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
-
----------------------------------------------------------------------------------------------------------------------------
-
-
-
-local Scanner = LoadCustomInstance("https://raw.githubusercontent.com/WShadowStudios/tablet/main/original_tablet.rbxm")
-Scanner.Parent = game.Players.LocalPlayer.Backpack
-
-local target_fps = _G.scanner_fps or 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-local disable_static = _G.disable_static or false
-
--- Variables
-local Storage = Scanner:WaitForChild("Storage")
-local Handle = Scanner:WaitForChild("Handle", 1)
-local ScannerViewportFrame = Storage.ScreenUI
-
-local ScannerActivateTickDelay = tick()
-
-local ScannerCamera = Instance.new("Camera")
-
-local TweenService = game:GetService("TweenService")
-local uis = game:GetService("UserInputService")
-local player = game.Players.LocalPlayer
-
-local surfacelight = Instance.new("SurfaceLight")
-surfacelight.Parent = Handle
-surfacelight.Face = "Back"
-surfacelight.Range = 80
-surfacelight.Angle = 70
-surfacelight.Shadows = true
-surfacelight.Enabled = false
-local showflv = false
-
-local LastScannedRoom = -1
-local CalculatedFPSWait = (1 / target_fps)
-
-local IsScannerOpened = false
-local Equipped = false
-
--- tables
-local ScannerObjectives = {
-    "KeyObtain",
-    "LeverForGate",
-    "LiveBreakerPolePickup",
-    "LiveHintBook",
-    "FuseObtain",
-    "MinesAnchor",
-    "WaterPump",
-    "TimerLever",
-    "LootHolder"
-}
-
-local StaticImageUrl = {
-    "rbxassetid://8681113666",
-    "rbxassetid://8681113503"
-}
-
-local LoadedAnimations = {}
-
-
--- Animation Functions
-local function ScannerStaticStart()
-	ScannerViewportFrame.OffScreen.Visible = false
-
-	ScannerViewportFrame.Vignette.ImageTransparency = 1
-	ScannerViewportFrame.Static1.ImageTransparency = 0
-	ScannerViewportFrame.Static2.ImageTransparency = 0
-
-	TweenService:Create(ScannerViewportFrame.Static1, TweenInfo.new(1, Enum.EasingStyle.Quart, Enum.EasingDirection.InOut), {
-		ImageTransparency = 0.98
-	}):Play()
-
-	TweenService:Create(ScannerViewportFrame.Static2, TweenInfo.new(1, Enum.EasingStyle.Quart, Enum.EasingDirection.InOut), {
-		ImageTransparency = 1
-	}):Play()
-
-	TweenService:Create(ScannerViewportFrame.Vignette, TweenInfo.new(1, Enum.EasingStyle.Quart, Enum.EasingDirection.InOut), {
-		ImageTransparency = 0.4
-	}):Play()
+if _G.OnShop then
+    local CustomShop = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Shop%20Items/Source.lua"))()
 	
-	ScannerViewportFrame.ViewSpecial.ImageColor3 = Color3.fromRGB(217, 255, 206)
-
-	TweenService:Create(ScannerViewportFrame.ViewSpecial, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.InOut, 10, true), {
-		ImageColor3 = Color3.fromRGB(53, 93, 52)
-	}):Play()
-
-
-    ScannerViewportFrame.ViewNormal.ImageColor3 = Color3.fromRGB(255, 249, 0)
-	ScannerCamera.FieldOfView = 1
-
-	TweenService:Create(ScannerCamera, TweenInfo.new(1, Enum.EasingStyle.Quart, Enum.EasingDirection.InOut), {
-		FieldOfView = 30
-	}):Play()
-
-	Scanner.Handle.Use:Play()
-	Scanner.Handle.Idle:Play()
+    CustomShop.CreateItem("", {
+        Title = "Scanner",
+        Desc = _G.Description or "",
+        Image = "https://static.wikia.nocookie.net/doors-game/images/4/4e/Scanner_Icon.png/revision/latest/scale-to-width-down/350?cb=20230203191128",
+        Price = _G.Price or 1000,
+        Stack = 1,
+    })
 end
 
-local function ScannerStaticStop()
-	ScannerViewportFrame.OffScreen.Visible = true
-	ScannerViewportFrame.OffScreen.Frame.Size = UDim2.new(1, 0, 1, 0)
-
-	TweenService:Create(ScannerViewportFrame.OffScreen.Frame, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {
-		Size = UDim2.new(1, 0, 0.1, 0)
-	}):Play()
-
-	task.delay(0.31, function()
-		TweenService:Create(ScannerViewportFrame.OffScreen.Frame, TweenInfo.new(0.4, Enum.EasingStyle.Quad, Enum.EasingDirection.In, 0), {
-			Size = UDim2.new(0, 0, 0.1, 0)
-		}):Play()
-	end)
-
-	ScannerViewportFrame.OffScreen.BackgroundColor3 = Color3.fromRGB(39, 59, 33)
-	TweenService:Create(ScannerViewportFrame.OffScreen, TweenInfo.new(2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out, 0), {
-		BackgroundColor3 = Color3.fromRGB(3, 6, 2)
-	}):Play()
-
-	Scanner.Handle.Disable:Play()
-	Scanner.Handle.Idle:Stop()
-end
-
-local function ScannerAnimateStatic()
-	ScannerViewportFrame.Static1.Position = UDim2.new(0.5, math.random(-28, 28) * 6, 0.5, math.random(-18, 18) * 6)
-	ScannerViewportFrame.Static2.Position = UDim2.new(0.5, math.random(-28, 28) * 6, 0.5, math.random(-18, 18) * 6)
-
-	local static_image = StaticImageUrl[math.random(1, #StaticImageUrl)]
-	ScannerViewportFrame.Static1.Image = static_image
-end
-
--- Essential Functions
-
-local function SetupScannerView(room)
-	local ScannerRoomView = Instance.new("Model", ScannerViewportFrame.ViewNormal)
-	ScannerRoomView.Name = room.Name
-
-	local function SetupCloneRoomPart(instance)
-		if instance:IsA("BasePart") and instance.Transparency ~= 1 and instance.Size.Magnitude > 0.2 then
-			local current_room_part = instance:Clone()
-
-			current_room_part.CanQuery = false
-			current_room_part.Parent = ScannerRoomView
-
-			if not instance.Anchored then
-				task.spawn(function()
-					while task.wait(0.5) do
-						if not instance or not instance.Parent then
-							break
-						end
-					
-						current_room_part.Position = instance.Position
-					end
-					
-					current_room_part:Destroy()
-				end)
-			end
-		end
-
-		if instance:IsA("Model") and table.find(ScannerObjectives, instance.Name) then
-			local StarObjective = Storage.Star:Clone()
-
-			StarObjective.CFrame = instance.PrimaryPart.CFrame or instance:GetPivot()
-			StarObjective.Parent = ScannerViewportFrame.ViewSpecial
-		end
-	end
-
-	-- clone of all visible parts in the room
-	for _, v in pairs(room:GetDescendants()) do
-		SetupCloneRoomPart(v)
-	end
-
-	local connection = room.DescendantAdded:Connect(function(part)
-		SetupCloneRoomPart(part)
-	end)
-
-	ScannerRoomView.AncestryChanged:Connect(function()
-		connection:Disconnect()
-	end)
-end
-
-local function CleanupScannerView()
-    for _, v in pairs(ScannerViewportFrame.ViewNormal:GetChildren()) do
-        if v:IsA("Model") then
-            v:Destroy()
-        end
-    end
-    
-    for _, v in pairs(ScannerViewportFrame.ViewSpecial:GetChildren()) do
-        if v:IsA("BasePart") then
-            v:Destroy()
-        end
-    end
-end
-
--- Init Scanner
-ScannerCamera.Parent = ScannerViewportFrame
-ScannerCamera.FieldOfView = 50
-
-ScannerViewportFrame.ViewNormal.CurrentCamera = ScannerCamera
-ScannerViewportFrame.ViewSpecial.CurrentCamera = ScannerCamera
-
-Scanner.Activated:Connect(function()
-	if not (ScannerActivateTickDelay <= tick()) then
-		return
-	end
-
-	if ScannerActivateTickDelay <= tick() then
-		ScannerActivateTickDelay = tick() + 0.5
-
-		LoadedAnimations.fire:Play(0.05, 1, 1)
-
-		task.wait(0.2)
-
-		IsScannerOpened = not IsScannerOpened
-
-		if not IsScannerOpened then
-			return ScannerStaticStop()
-		end
-	end
-
-	ScannerStaticStart()
-end)
-
-Scanner.Equipped:Connect(function()
-	for _, anim in pairs(Scanner:WaitForChild("Animations"):GetChildren()) do
-		LoadedAnimations[anim.Name] = player.Character.Humanoid:LoadAnimation(anim)
-	end
-
-	LoadedAnimations.equip:Play()
-	LoadedAnimations.idle:Play()
-
-	ScannerViewportFrame.Parent = player.PlayerGui
-	ScannerViewportFrame.Enabled = true
-	ScannerViewportFrame.Adornee = Scanner:WaitForChild("Handle"):WaitForChild("Screen")
-
-	local target_room = player:GetAttribute("CurrentRoom")
-	LastScannedRoom = target_room
-
-	local room_instance = workspace.CurrentRooms:FindFirstChild(target_room)
-	if room_instance and ScannerViewportFrame.ViewNormal:FindFirstChild(target_room) == nil then
-		SetupScannerView(room_instance)
-	end
-
-	task.wait(0.2)
-
-	Equipped = true
-	IsScannerOpened = true
-
-	ScannerStaticStart()
-
-	while Equipped do
-		if IsScannerOpened then
-			-- NO WAY ITS MSTDUIO45 😨😨
-			local success, errormsg = pcall(function()
-				target_room = player:GetAttribute("CurrentRoom")
-
-				if not disable_static then
-					ScannerCamera.CFrame = Scanner.Handle.Screen.CFrame * CFrame.Angles(0, 3.15, 0)
-					ScannerViewportFrame.ViewNormal.LightDirection = ScannerCamera.CFrame.LookVector - Vector3.new(0, 1, 0)
-					
-					ScannerAnimateStatic()
-				end
-
-				if disable_static and not ScannerViewportFrame.Static1.Visible then
-					ScannerViewportFrame.Static1.Visible = false
-					ScannerViewportFrame.Static2.Visible = false
-				end
-
-				for _, v in pairs(ScannerViewportFrame.ViewSpecial:GetChildren()) do
-					if v:IsA("BasePart") then
-						v.CFrame = CFrame.new(v.Position, ScannerCamera.CFrame.Position)
-					end
-				end
-
-				if target_room ~= LastScannedRoom then
-					LastScannedRoom = target_room
-
-					ScannerStaticStart()
-					CleanupScannerView()
-
-					room_instance = workspace.CurrentRooms:FindFirstChild(target_room)
-					if room_instance and ScannerViewportFrame.ViewNormal:FindFirstChild(target_room) == nil then
-						SetupScannerView(room_instance)
-					end
-				end
-			end)
+local Scanner = game:GetObjects("rbxassetid://12594482248")[1]
+Scanner.Parent = game.Players.LocalPlayer.Backpack
+local Storage = Scanner:WaitForChild("Storage")
+local l__ScreenUI__1 = Storage.ScreenUI;
+local v3 = Instance.new("Camera");
+v3.Parent = l__ScreenUI__1;
+v3.FieldOfView = 50;
+l__ScreenUI__1.ViewNormal.CurrentCamera = v3;
+l__ScreenUI__1.ViewSpecial.CurrentCamera = v3;
+local v4 = {
+	anim = {}
+};
+local l__TweenService__1 = game:GetService("TweenService");
+local u2 = {};
+local l__LocalPlayer__3 = game.Players.LocalPlayer;
+local u4 = -1;
+local u5 = -1;
+local function u6(p1)
+	local v5 = Instance.new("Model");
+	v5.Name = p1.Name;
+	v5.Parent = l__ScreenUI__1.ViewNormal;
+	for _, v10 in pairs(p1:GetDescendants()) do
+		if v10:IsA("BasePart") and v10.Transparency ~= 1 and v10.Size.Magnitude > 0.2 then
+			local v11 = v10:Clone();
+			v11.CanQuery = false;
+			v11.Parent = v5;
 			
-			if errormsg then
-				warn(errormsg)
-			end
+			if v10.Anchored == false then
+				task.spawn(function()
+					while true do
+						wait(0.5);
+						if not v10 then
+							break;
+						end;
+						if not v10.Parent then
+							break;
+						end;
+						v11.Position = v10.Position;					
+					end;
+					v11:Destroy();
+				end);
+			end;
 		end
-
-		if not Equipped then
-			break
-		end
-
-		task.wait(CalculatedFPSWait)
+		if v10:IsA("Model") and (v10.Name == "KeyObtain" or v10.Name == "LeverForGate" or v10.Name == "LiveBreakerPolePickup" or v10.Name == "LiveHintBook") then
+			local v12 = Storage.Star:Clone();
+			v12.CFrame = v10.PrimaryPart.CFrame;
+			v12.Parent = l__ScreenUI__1.ViewSpecial;
+		end;
 	end
-end)
-
-Scanner.Unequipped:Connect(function()
-	ScannerViewportFrame.Parent = script
-	ScannerViewportFrame.Enabled = false
-
-	Equipped = false
-
-	-- Disable all sounds
-	for _, v in pairs(Handle:GetChildren()) do
-		if v:IsA("Sound") then
-			v:Stop()
-		end
-	end
-
-	Scanner.Handle.Disable:Play()
-
-	CleanupScannerView()
-
-	LoadedAnimations.equip:Stop()
-	LoadedAnimations.idle:Stop()
-end)
-
-uis.InputBegan:Connect(function(input)
- if input.KeyCode == Enum.KeyCode.F then
-   if showflv == false then
-     surfacelight.Enabled = true
-     showflv = true
-    elseif showflv == true then
-     surfacelight.Enabled = false
-     showflv = false
-end
-end
-end)
-				
+	local u7 = p1.DescendantAdded:Connect(function(p2)
+		if p2:IsA("BasePart") and p2.Transparency ~= 1 and p2.Anchored == true and p2.Size.Magnitude > 0.2 then
+			local v13 = p2:Clone();
+			v13.CanQuery = false;
+			v13.Parent = v5;
+			if p2.Anchored == false then
+				task.spawn(function()
+					while true do
+						wait(0.5);
+						if not p2 then
+							break;
+						end;
+						if not p2.Parent then
+							break;
+						end;
+						v13.Position = p2.Position;					
+					end;
+					v13:Destroy();
+				end);
+			end;
+		end;
+	end);
+	v5.AncestryChanged:Connect(function()
+		u7:Disconnect();
+	end);
+end;
+local u8 = true;
+local u9 = true;
+local function u10()
+	l__ScreenUI__1.OffScreen.Visible = false;
+	l__ScreenUI__1.Static1.ImageTransparency = 0;
+	l__ScreenUI__1.Static2.ImageTransparency = 0;
+	l__TweenService__1:Create(l__ScreenUI__1.Static1, TweenInfo.new(3, Enum.EasingStyle.Quart, Enum.EasingDirection.InOut), {
+		ImageTransparency = 0.9
+	}):Play();
+	l__TweenService__1:Create(l__ScreenUI__1.Static2, TweenInfo.new(3, Enum.EasingStyle.Quart, Enum.EasingDirection.InOut), {
+		ImageTransparency = 1
+	}):Play();
+	l__ScreenUI__1.ViewSpecial.ImageColor3 = Color3.fromRGB(217, 255, 206);
+	l__TweenService__1:Create(l__ScreenUI__1.ViewSpecial, TweenInfo.new(0.2, Enum.EasingStyle.Quart, Enum.EasingDirection.InOut, 10, true), {
+		ImageColor3 = Color3.fromRGB(53, 93, 52)
+	}):Play();
+	v3.FieldOfView = 1;
+	l__TweenService__1:Create(v3, TweenInfo.new(3, Enum.EasingStyle.Quart, Enum.EasingDirection.InOut), {
+		FieldOfView = 30
+	}):Play();
+	Scanner.Handle.Use:Play();
+	Scanner.Handle.Idle:Play();
+end;
+function equip(p3)
+	print("scanner module: tool equipped!");
+	for v14, v15 in pairs(Scanner:WaitForChild("Animations"):GetChildren()) do
+		u2[v15.Name] = l__LocalPlayer__3.Character.Humanoid:LoadAnimation(v15);
+	end;
+	u2.equip:Play();
+	u2.idle:Play();
+	l__ScreenUI__1.Parent = l__LocalPlayer__3.PlayerGui;
+	l__ScreenUI__1.Enabled = true;
+	l__ScreenUI__1.Adornee = Scanner:WaitForChild("Handle"):WaitForChild("Screen");
+	u4 = l__LocalPlayer__3:GetAttribute("CurrentRoom");
+	u5 = u4;
+	for v16 = u4, u4 do
+		local v17 = workspace.CurrentRooms:FindFirstChild(v16);
+		if v17 and l__ScreenUI__1.ViewNormal:FindFirstChild(v16) == nil then
+			u6(v17);
+		end;
+	end;
+	wait(0.2);
+	u8 = true;
+	u9 = true;
+	u10();
+	while u8 do
+		if u9 == true then
+			local v18, v19 = pcall(function()
+				v3.CFrame = Scanner.Handle.Screen.CFrame * CFrame.Angles(0, 3.141592653589793, 0);
+				l__ScreenUI__1.ViewNormal.LightDirection = v3.CFrame.LookVector - Vector3.new(0, 1, 0);
+				l__ScreenUI__1.Static1.Position = UDim2.new(0.5, math.random(-28, 28) * 6, 0.5, math.random(-18, 18) * 6);
+				l__ScreenUI__1.Static2.Position = UDim2.new(0.5, math.random(-28, 28) * 6, 0.5, math.random(-18, 18) * 6);
+				if math.random(1, 2) == 2 then
+					v20 = "rbxassetid://8681113666";
+				else
+					v20 = "rbxassetid://8681113503";
+				end;
+				l__ScreenUI__1.Static1.Image = v20;
+				u4 = l__LocalPlayer__3:GetAttribute("CurrentRoom");
+				for v21, v22 in pairs(l__ScreenUI__1.ViewSpecial:GetChildren()) do
+					if v22:IsA("BasePart") then
+						v22.CFrame = CFrame.new(v22.Position, v3.CFrame.p);
+					end;
+				end;
+				if u4 ~= u5 then
+					u5 = u4;
+					u10();
+					for v23, v24 in pairs(l__ScreenUI__1.ViewNormal:GetChildren()) do
+						if v24:IsA("Model") then
+							v24:Destroy();
+						end;
+					end;
+					for v25, v26 in pairs(l__ScreenUI__1.ViewSpecial:GetChildren()) do
+						if v26:IsA("BasePart") then
+							v26:Destroy();
+						end;
+					end;
+					for v27 = u4, u4 do
+						local v28 = workspace.CurrentRooms:FindFirstChild(v27);
+						if v28 and l__ScreenUI__1.ViewNormal:FindFirstChild(v27) == nil then
+							u6(v28);
+						end;
+					end;
+				end;
+			end);
+			if v19 then
+				warn(v19);
+			end;
+		end;
+		if u8 == false then
+			break;
+		end;
+		task.wait(0.033444816053511704);	
+	end;
+end;
+local u11 = tick();
+local function u12()
+	l__ScreenUI__1.OffScreen.Visible = true;
+	l__ScreenUI__1.OffScreen.Frame.Size = UDim2.new(1, 0, 1, 0);
+	l__TweenService__1:Create(l__ScreenUI__1.OffScreen.Frame, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {
+		Size = UDim2.new(1, 0, 0.1, 0)
+	}):Play();
+	delay(0.31, function()
+		l__TweenService__1:Create(l__ScreenUI__1.OffScreen.Frame, TweenInfo.new(0.4, Enum.EasingStyle.Quad, Enum.EasingDirection.In, 0), {
+			Size = UDim2.new(0, 0, 0.1, 0)
+		}):Play();
+	end);
+	l__ScreenUI__1.OffScreen.BackgroundColor3 = Color3.fromRGB(39, 59, 33);
+	l__TweenService__1:Create(l__ScreenUI__1.OffScreen, TweenInfo.new(2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out, 0), {
+		BackgroundColor3 = Color3.fromRGB(3, 6, 2)
+	}):Play();
+	Scanner.Handle.Disable:Play();
+	Scanner.Handle.Idle:Stop();
+end;
+function fire()
+	if u11 <= tick() then
+		u11 = tick() + 0.5;
+		u2.fire:Play(0.05, 1, 1);
+		wait(0.2);
+		u9 = not u9;
+		if u9 ~= true then
+			u12();
+			return;
+		end;
+	else
+		return;
+	end;
+	u10();
+end;
+local l__Handle__13 = Scanner:WaitForChild("Handle", 1);
+function unequip(p4)
+	print("scanner module: tool unequipped!");
+	l__ScreenUI__1.Parent = script;
+	l__ScreenUI__1.Enabled = false;
+	u8 = false;
+	for v29, v30 in pairs(l__Handle__13:GetChildren()) do
+		if v30:IsA("Sound") then
+			v30:Stop();
+		end;
+	end;
+	Scanner.Handle.Disable:Play();
+	for v31, v32 in pairs(l__ScreenUI__1.ViewNormal:GetChildren()) do
+		if v32:IsA("Model") then
+			v32:Destroy();
+		end;
+	end;
+	u2.equip:Stop();
+	u2.idle:Stop();
+end;
+Scanner.Activated:Connect(fire);
+Scanner.Equipped:Connect(equip);
+Scanner.Unequipped:Connect(unequip);
